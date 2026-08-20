cf6e652a55019880404c0d36baef8d44370e280f
Tia <test@test.com>
2026-08-20 13:17:45 +0100

    upload changes to metric work

---
+++ Final_Code_Metrics.py
@@ -0,0 +1,232 @@
+#Import Libraries
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import time
+
+#Define Functions
+
+# Metrics storage
+etl_metrics = []
+
+def log_metrics(dataset_name, records_received, records_processed):
+    records_dropped = records_received - records_processed
+
+    etl_metrics.append({
+        "Run Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "Dataset": dataset_name,
+        "Records Received": records_received,
+        "Records Processed": records_processed,
+        "Records Dropped": records_dropped
+    })
+
+
+##Checks the loads the files, will show errors
+def fileLoader(file_path):
+    try:
+        if file_path.lower().endswith('.csv'):
+            return pd.read_csv(file_path)
+        elif file_path.lower().endswith(('.xlsx', '.xls')):
+            return pd.read_excel(file_path)
+        else:
+            raise ValueError("Unsupported file type")
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"Error loading file: {e}")
+        return None
+
+#Counts the number of duplicates then drops them if there are 1 or more.
+def duplicateCheck(df):
+    duplicate_count = df.duplicated().sum()
+    print(f"Duplicate rows found: {duplicate_count}")
+
+    if duplicate_count > 0:
+        df = df.drop_duplicates()
+        print(f"Duplicates removed. New row count: {len(df)}")
+
+    return df, duplicate_count
+
+#Counts the number of na then drops them if there are 1 or more.
+def naCheck(df):
+    na_count = df.isna().sum().sum()
+    print(f"Missing values found: {na_count}")
+    if na_count > 0:
+        df = df.dropna()
+        print(f"Rows containing NAs removed. New row count: {len(df)}")
+    return df
+
+
+def datediff(ColA, ColB, df):
+    df['Days Borrowed'] = (df[ColA]-df[ColB]).dt.days
+    return df
+
+#Main Code
+
+if __name__ == "__main__":
+    print("Start")
+   
+    #Capture Start Time Metric
+    start_time = time.time()
+
+    #Import raw data
+    df_system_book = fileLoader(r"C:\Users\Admin\Desktop\Module-5\DATA\03_Library Systembook.csv")
+    df_system_customers =fileLoader(r"C:\Users\Admin\Desktop\Module-5\DATA\03_Library SystemCustomers.csv")
+
+    #Caputer Metric 1: row count
+    book_records_received = len(df_system_book)
+    customer_records_received = len(df_system_customers)
+
+    #DATA SET 1- CLEANING
+    ###Remove "" from Book checkout column and format as Date
+    df_system_book["Book checkout"] = df_system_book["Book checkout"].str.strip('""')
+    df_system_book["Book checkout"] = pd.to_datetime(df_system_book["Book checkout"],dayfirst = True, errors = "coerce")
+    df_system_book["Book checkout_Formated"] = df_system_book["Book checkout"].dt.strftime("%d/%m/%Y")
+
+    ###Format from Book Returned column
+    df_system_book["Book Returned"] = pd.to_datetime(df_system_book["Book Returned"],dayfirst = True, errors = "coerce")
+    df_system_book["Book Returned_Formated"] = df_system_book["Book Returned"].dt.strftime("%d/%m/%Y")
+
+    ##Remove weeks from days allowed to borrow and then x7 to return number of days
+    df_system_book["Days allowed to borrow"] = df_system_book["Days allowed to borrow"].str.strip('weeks')
+    df_system_book["Days allowed to borrow"] = pd.to_numeric(df_system_book["Days allowed to borrow"])*7
+    df_system_book["Days allowed to borrow"] = (df_system_book["Days allowed to borrow"].round(0).astype("Int64"))
+
+    ## Sort out remaining column formats
+    df_system_book["Id"] = df_system_book["Id"].astype("Int64")
+    df_system_book["Customer ID"] = df_system_book["Customer ID"].astype("Int64")
+
+    df_system_book["Books"] = df_system_book["Books"].astype("string")
+    
+    ##Caputer nulls before removing 
+    error_log = df_system_book[df_system_book.isnull().any(axis=1)].copy()
+    
+    ##Remove all nulls
+    df_system_book = naCheck(df_system_book)
+
+    ##Change the word NaT to nan
+    df_system_book["Book checkout"] = df_system_book["Book checkout"].replace(pd.NaT, np.nan)
+
+    ##Remove those that do not have a checkout date, but these are in the error log to fix
+    df_system_book = df_system_book[df_system_book["Book checkout"].notna() ]
+    
+    ##check Duplicates
+    df_system_book, book_duplicate_rows_removed = duplicateCheck(df_system_book)
+
+    #DATA SET 1- ENIRCHING
+    ##Calculate number of days borrowed
+    df_system_book= datediff(df = df_system_book, ColA ="Book Returned", ColB= "Book checkout")
+  
+    ##Estalish if the book was returned on time or late
+    df_system_book["Status"] = np.where(
+    df_system_book["Book Returned"].isna(), "On Loan", np.where(
+    df_system_book["Days Borrowed"] > df_system_book["Days allowed to borrow"], "Returned Late", "Returned On Time"))
+
+    print(df_system_book["Status"].value_counts())
+   
+    #DATA SET 1- VALIDATION & ERROR LOGGING
+
+    ##Review how many rows still contain null
+    df_system_book.isnull().sum()
+    book_null_rows_removed = (df_system_book.isnull().any(axis=1).sum())
+
+    #Data METRICS
+    book_records_processed = len(df_system_book)
+    book_records_dropped = book_records_received - book_records_processed
+    book_null_rows_removed = len(error_log)
+
+    ##Create Error log -Could be developed further to add dates it was found and reason why its an error.
+    ##error_log = df_system_book[df_system_book.isnull().any(axis=1)].copy()
+    error_log.to_csv("03_Library Systembook_ERROR_LOG.csv", index=False)
+    print("03_Library Systembook_ERROR_LOG File saved")
+    
+
+    #DATA SET 1- CLEAN OUTPUT
+    df_system_book.to_csv(
+    "03_Library Systembook_CLEAN.csv",
+    index=False)
+    print("03_Library Systembook_CLEAN File saved")
+
+    ##Metrics Output
+    etl_metrics.append({
+    "Run Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "Dataset": "Library System Book",
+    "Records Received": book_records_received,
+    "Records Processed": book_records_processed,
+    "Records Dropped": book_records_dropped,
+    "Null Rows Removed": book_null_rows_removed,
+    "Duplicate Rows Removed": book_duplicate_rows_removed})
+
+
+    #DATA SET 2- CLEANING
+    ##Remove all nulls
+    df_system_customers = naCheck(df_system_customers)
+    customer_null_rows_removed = (customer_records_received - len(df_system_customers))
+   ## print("After naCheck:", type(df_system_customers))
+
+    ##check Duplicates
+   # df_system_customers = duplicateCheck(df_system_customers)
+    #df_system_customers, customer_duplicate_rows_removed = duplicateCheck(df_system_customers)
+    df_system_customers, customer_duplicate_rows_removed = duplicateCheck(df_system_customers)
+  
+  ##  print("After duplicateCheck:", type(df_system_customers))
+
+    #Data METRICS
+    customer_records_processed = len(df_system_customers)
+    customer_records_dropped = (customer_records_received - customer_records_processed)
+
+    #DATA SET 2- VALIDATION & ERROR LOGGING
+    print(df_system_customers)
+    print(type(df_system_customers))
+
+    print(type(df_system_customers))
+    ##ID Validation Check
+    expected_ids = range(
+    int(df_system_customers["Customer ID"].min()),
+    int(df_system_customers["Customer ID"].max()) + 1 )
+    missing_ids = set(expected_ids) - set(df_system_customers["Customer ID"])
+    print(missing_ids)
+   
+   ##Log missing IDS for review
+    missing_customer_ids = pd.DataFrame({
+    "Missing Customer ID": list(missing_ids)})
+    missing_customer_ids["Error Reason"] = "Customer record missing"
+    missing_customer_ids.to_csv(
+    "03_Library SystemCustomers_ERROR_LOG.csv",
+    index=False
+    )
+    print("Library System Customer Error Log saved")
+    
+    ##Metrics Output
+    etl_metrics.append({
+    "Run Date": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    "Dataset": "Library System Customers",
+    "Records Received": customer_records_received,
+    "Records Processed": customer_records_processed,
+    "Records Dropped": customer_records_dropped,
+    "Null Rows Removed": customer_null_rows_removed,
+    "Duplicate Rows Removed": customer_duplicate_rows_removed})
+
+   #DATA SET 2- CLEAN OUTPUT
+    df_system_customers.to_csv(
+    "03_Library SystemCustomers_CLEAN.csv",
+    index=False)
+
+    print("Clean Library System Customer Data File saved")
+
+    #Save and Output METRICs
+    metrics_df = pd.DataFrame(etl_metrics)
+
+    #Calculate run time
+    end_time = time.time()
+    run_time_seconds = round(end_time - start_time, 2)
+    print(f"Pipeline Run Time: {run_time_seconds} seconds")
+
+    #Add run time metrics to table and output to csv
+    metrics_df["Run Time (Seconds)"] = run_time_seconds
+    metrics_df.to_csv("03_ETL_Metrics.csv", index=False)
+    
+    print("ETL Metrics File saved")
+    
+    print("END")
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Module-5
-Data Engineering Produce Development Module 5
-Tia
+Data Engineering Product Development Module 5 - Tia
+
+
+## Project Problem Summary
+This project was developed to improve the quality, reliability, and efficiency of a Library's data processing activities. The existing process relied on manual data quality checks, which were time-consuming and prone to inconsistencies. To address this, an automated ETL (Extract, Transform, Load) solution was created using Python to cleanse, validate, transform, and prepare data for reporting and analysis.
+
+## Solution Proposal
+The solution processes Library Book and Customer datasets, performing data quality checks such as identifying and removing null values, detecting duplicate records, validating customer records, and generating error logs for records requiring further investigation. Cleaned datasets and ETL perofrmance metrics are then exported for use in downstream reporting processes.
+
+## ETL Pipeline
+![alt text](image-1.png)
+
+
+## Metrics Dashboard
+
+![alt text](image.png)
